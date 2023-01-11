26070bbb0c319c376dcf609e926a7b556838c81c
borowcze <adamborowiec7@gmail.com>
2023-01-11 22:16:12 +0100

    Organism model and main menu

--- src/main/java/org/borowcze/Main.java
+++ src/main/java/org/borowcze/Main.java
@@ -9,16 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         World.runGame();
 
-        System.out.println("Hello world!");
-        ArrayList<Point> arrayList = new ArrayList<>();
-        arrayList.add(new Point(0,1));
-        arrayList.add(new Point(1,2));
-        arrayList.add(new Point(3,5));
 
-        Random random = new Random();
-        int randomNumber = random.nextInt(arrayList.size());
-        System.out.println(randomNumber);
-        System.out.println(arrayList.get(randomNumber));
-        System.out.println(arrayList.get(randomNumber).x +" "+ arrayList.get(randomNumber).y);
     }
 }
